20d81c58940855ad6f3d4b5476a4cdf7b975490d
Robbie Hayward <Robbie@imac.home>
2017-12-04 20:36:03 +0000

    using objects

--- nqueens.js
+++ nqueens.js
@@ -2,32 +2,69 @@ var boardSize = 4; //set square size
 
 var solutions = 0;
 var array = [];
+var maxVal = (boardSize-1);
 
-function init() {
-	if (!anyMatch(array.length, y)) {
-		array.push(y);
+function init(x,y) {
+	while (x<boardSize) {
+		while (y<boardSize) {
+			console.log(x,y);
+			var point = new Point(x,y);
+			point.compare();
+			y++;
+		}
+		y=0;
+		x++;
+	}
+	cycle();
+}
+
+function cycle() {
+	if (array.length<maxVal) {
+		var pop = array.pop();
+		init(pop.x,pop.y);
 	}
 }
 
+function Point(x,y) {
+	this.x = x;
+	this.y = y;
+	this.index = false;
+	this.compare = function () {
+		if (!anyMatch(this.x,this.y)) {
+			this.add();
+			console.log(array);
+		};
+	};
+	this.add = function () {
+		array.push(this);
+		this.index = array.indexOf(this);
+	};
+	this.del = function () {
+		array.splice(this.index,1)
+	};
+}
+
 function anyMatch(x,y) {
-	if (yMatch(y) || diaMatch(x,y)) {
+	if (lineMatch(x,y) || diaMatch(x,y)) {
 		return true;
 	}
 	return false;
 }
 
-function yMatch(y) {
-	if (array.includes(y)) {
-		return true;
+function lineMatch(x,y) {
+	for (i=0;i<array.length;i++) {
+		if (array[i].x==x || array[i].y==y) {
+			return true;
+		}
 	}
 	return false;
 }
 
 function diaMatch(x,y) {
 	for (i=0;i<array.length;i++) {
-		if (Math.abs(x - i) == Math.abs(y - array[i])) {
+		if (Math.abs(x - array[i].x) == Math.abs(y - array[i].y)) {
 			return true;
 		}
-		return false;
 	}
+	return false;
 }
